3eb8ba197d9a68207978d9f87c1fc25ba7098b3f
Gregory Frye <Swayze@Gregorys-MacBook-Pro.local>
2018-09-12 18:45:55 -0500

    Commit 1 - Easy Going answered

---
+++ app.js
@@ -0,0 +1,18 @@
+//console.log('hello')
+
+
+
+//Easy Going
+ // for( let q = 1; q <= 20; q = q + 1) {
+ // 	console.log(q);
+ // }
+
+// Get Even
+ for( let q = 0; q <= 200; q++){
+ 	if((q % 2) === 0) { 
+ 		console.log(q)
+
+ 	}
+}
+
+
