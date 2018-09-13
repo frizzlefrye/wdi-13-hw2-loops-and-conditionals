78d308b72c1f4600daf7ec4b4631a3ab4a420a26
Gregory Frye <Swayze@Gregorys-MacBook-Pro.local>
2018-09-12 20:04:43 -0500

    Commit 3 - Excited Kittens answered

--- app.js
+++ app.js
@@ -8,12 +8,31 @@
  // }
 
 // Get Even- finished problem
- for( let q = 0; q <= 200; q++){
- 	if((q % 2) === 0) { 
- 		console.log(q)
+//  for( let q = 0; q <= 200; q++){
+//  	if((q % 2) === 0) { 
+//  		console.log(q)
 
- 	}
+//  	}
+// }
+
+//Excited Kitten
+
+ 
+
+for(let i = 1; i < 20; i++) {
+	const meowArr = ["...human...why you taking pictures of me?...", "...the catnip made me do it...","...why does the red dot always get away..."];
+	const randomValue = meowArr[Math.floor(meowArr.length * Math.random())];
+
+	
+ if( i % 2 === 0) { 
+
+ 	console.log(randomValue);
+
+
+ 	} else { 
+	console.log("Love me, pet me! HSSSSSS!")
+ }
 }
 
 
-
+	
