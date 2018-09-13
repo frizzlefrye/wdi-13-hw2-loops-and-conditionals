dda31d878e3b3dd70b26e5b60dd91545747efd36
Gregory Frye <Swayze@Gregorys-MacBook-Pro.local>
2018-09-13 18:37:26 -0500

    Commit 7 - Kristyn and Thom have their outfits ready for class - array practice

--- app.js
+++ app.js
@@ -46,22 +46,24 @@
 
 // }
 
-const shahzad = ["Shahzad", 1000, "Austin"];
-const jim = ["Jim", 16, "All cities"];
-const reuben = ["Reuben", 22, "Durham"];
-const daniel = ["Daniel", 186, "Dallas"];
-const ryan = ["Ryan", 65, "Denver"];
+// Getting to know you
+
+// const shahzad = ["Shahzad", 1000, "Austin"];
+// const jim = ["Jim", 16, "All cities"];
+// const reuben = ["Reuben", 22, "Durham"];
+// const daniel = ["Daniel", 186, "Dallas"];
+// const ryan = ["Ryan", 65, "Denver"];
 
 
 
-shahzad[0] = "Gameboy";
-daniel[1] = daniel[1] + 1;
-ryan[2] = "Gotham City";
-reuben.pop();
-reuben.push("Chicago");
- jim.splice(2,1,"Tampa", "South Bend", "Denver")
+// shahzad[0] = "Gameboy";
+// daniel[1] = daniel[1] + 1;
+// ryan[2] = "Gotham City";
+// reuben.pop();
+// reuben.push("Chicago");
+//  jim.splice(2,1,"Tampa", "South Bend", "Denver")
 
-console.log(jim)
+// console.log(jim)
 
 
 
@@ -76,7 +78,75 @@ console.log(jim)
 // }	
 // add uppercase to console.log!
 
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
+const kristynsShoe = [];
+kristynsShoe.push(kristynsCloset[0])
+kristynsCloset.splice(0,1);
+
+
+console.log(kristynsShoe)
+
+// Dress Em Up
+// I had a lot of help with this
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
+
+
+
+const krystOut1 = "Kristyn's gonna wear a " + kristynsCloset[2] + "," + kristynsCloset[1] + ", and " + kristynsCloset[5];
+console.log(krystOut1);
+
+const krystOut2 ="Kristyn's gonna wear a " + kristynsCloset[3] + "," + kristynsCloset[4] + ", and " + kristynsCloset[6];
+console.log(krystOut2);
+
+const krystOut3 ="Kristyn's gonna wear a " + kristynsCloset[0] + "," + kristynsCloset[5] + ", and " + kristynsCloset[4]; 
+console.log(krystOut3);
+
+
+const thomOut1 = "Thom's gonna wear a " + thomsCloset[0][3] + "," + thomsCloset[2][2] + ", and " +thomsCloset[1][1];
+console.log(thomOut1);
+
+const thomOut2 = "Thom's gonna wear a " + thomsCloset[0][2] + "," + thomsCloset[1][0] + ", and " +thomsCloset[2][0];
+console.log(thomOut2);
+
+const thomOut3 = "Thom's gonna wear a " + thomsCloset[0][0] + "," + thomsCloset[1][2] + ", and " +thomsCloset[2][1];
+console.log(thomOut3);
+
+
 
 
 	
 	
+
+
+
+
+
+
+
+
