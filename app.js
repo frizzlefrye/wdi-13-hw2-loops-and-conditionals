c595588eb90cd39d1c31cdfc1349bb996c569a91
Gregory Frye <Swayze@Gregorys-MacBook-Pro.local>
2018-09-12 20:23:39 -0500

    Commit 4 - Fizz Buzz answered

--- app.js
+++ app.js
@@ -17,22 +17,40 @@
 
 //Excited Kitten
 
- 
-
-for(let i = 1; i < 20; i++) {
-	const meowArr = ["...human...why you taking pictures of me?...", "...the catnip made me do it...","...why does the red dot always get away..."];
-	const randomValue = meowArr[Math.floor(meowArr.length * Math.random())];
+// for(let i = 1; i < 20; i++) {
+// 	const meowArr = ["...human...why you taking pictures of me?...", "...the catnip made me do it...","...why does the red dot always get away..."];
+// 	const randomValue = meowArr[Math.floor(meowArr.length * Math.random())];
 
 	
- if( i % 2 === 0) { 
+//  if( i % 2 === 0) { 
 
- 	console.log(randomValue);
+//  	console.log(randomValue);
 
 
- 	} else { 
-	console.log("Love me, pet me! HSSSSSS!")
- }
+//  	} else { 
+// 	console.log("Love me, pet me! HSSSSSS!")
+//  }
+// }
+
+
+for(let i = 1; i <= 100; i++)
+	// if the number is divisible by 3 and 5, write "FizzBuzz"
+if (i % 3 === 0 && i % 5 === 0){
+console.log("FizzBuzz");
+} else 
+// if the number is divisible by 3, write "Fizz"
+if ( i % 3 === 0 ) { 
+console.log("Fizz");
+}
+else if (i % 5 ===0)
+// if the number is divisible by 5, write "Buzz"
+{
+console.log("Buzz");
+}
+// otherwise, write just the number
+else {
+console.log(i);
 }
 
-
+	
 	
